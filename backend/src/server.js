43334708bc77fc61a06321014f29f904a1fe8d09
YanRicardo-Afk <yanricardo11@gmail.com>
2026-05-27 22:08:58 -0300

    Adicionei o nodemon, para que a gente não precise ficar recarregando a pagina toda hora para ver as alterações, so vamos precisar dar "npm run dev" e entrar no local host

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,5 +4,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
+
+console.log('Servidor iniciado');
