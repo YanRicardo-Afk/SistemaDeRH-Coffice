eab08b3558f1d3ee72ea153d35be741df290c07a
YanRicardo-Afk <yanricardo11@gmail.com>
2026-07-16 11:15:54 -0300

    O menu e a navbar agr se adaptam melhor no mobile

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -29,3 +29,36 @@ document
         }
 
     });
+
+    const btnMenuMobile =
+    document.getElementById("btnMenuMobile");
+
+const menu =
+    document.querySelector(".menu");
+
+const overlay =
+    document.getElementById("overlayMenu");
+
+if(btnMenuMobile && menu){
+
+    btnMenuMobile.addEventListener("click",()=>{
+
+        menu.classList.toggle("aberto");
+
+        overlay.classList.toggle("ativo");
+
+    });
+
+}
+
+if(overlay){
+
+    overlay.addEventListener("click",()=>{
+
+        menu.classList.remove("aberto");
+
+        overlay.classList.remove("ativo");
+
+    });
+
+}
